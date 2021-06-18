1389e8495b1bd9fffd5b0ff562da49ccbba8d6e4
ebeaury <ebeaury@umass.edu>
2021-06-18 13:26:54 -0400

    Added code to incorporate NCEAS datasets

--- Combining datasets.R
+++ Combining datasets.R
@@ -19,6 +19,10 @@ imap_cov = read.csv("iMap_pctcov_4_30_2021.csv")
 nwca = read.csv("NWCA_all_introduced_2011.csv")
 tx = read.csv("TX_all_occurrences_5_22_2021.csv")
 vahnp = read.csv("VANHP_reduced_06_16_21_EB.csv")
+nps = read.csv("NPS.AllSpTraits_21April2021.csv")
+blm = read.csv("AIM_AllSpTax_24June2020.csv")
+fia = read.csv("FIA Veg data Latest Traits 8-4-20.csv")
+neon = read.csv("NEONdata_flatted20210225traits.csv")
 
 ## Reformating to matching columns and different types of cover measurements
 # vegbank
@@ -70,12 +74,42 @@ unique(tx_sub$Cover)
 #vanhp
 glimpse(vahnp)
 vahnp_sub = vahnp %>% filter(ExoticStatus=="I") %>% select(UniqueID, Dataset, Long, Lat, SpCode, PctCov) %>%
-  rename(Cover = PctCov) %>% mutate(CoverType = "PercentCover")
+  rename(Cover = PctCov) %>% mutate(CoverType = "PercentCover") %>% distinct()
 glimpse(vahnp_sub)
 range(vahnp_sub$Cover)
+# nps
+glimpse(nps)
+nps_sub = nps %>% filter(Exotic=="I") %>%
+  select(UniqueID, Dataset, Long, Lat, Species, Pct_Cov) %>%
+  rename(Cover=Pct_Cov, SpCode = Species) %>% mutate(CoverType = "PercentCover") %>% distinct()
+glimpse(nps_sub)
+range(nps_sub$Cover)
+# fia
+glimpse(fia)
+fia_sub = fia %>% mutate(Dataset="FIA", UniqueID = paste("FIA", PLOT, sep="_")) %>% filter(inv_L48=="I") %>%
+  select(UniqueID, Dataset, LON, LAT, VEG_SPCD, PlotCover) %>%
+  rename(Cover=PlotCover, Long = LON, Lat = LAT, SpCode =VEG_SPCD) %>% mutate(CoverType = "PercentCover") %>% distinct()
+glimpse(fia_sub)
+range(fia_sub$Cover)
+# blm
+glimpse(blm)
+blm_sub = blm %>% mutate(Dataset = DataSet, UniqueID = PLOTKEY, Cover = prop.cover*100) %>%
+  select(UniqueID, Dataset, NAD83.Y, NAD83.X, code, Cover) %>%
+  rename(SpCode=code, Long = NAD83.X, Lat = NAD83.Y) %>% mutate(CoverType = "PercentCover") %>% distinct()
+glimpse(blm_sub)
+range(blm_sub$Cover)
+# neon
+glimpse(neon)
+neon_sub = neon %>% filter(Native.Status=="I") %>%
+  select(dataset,plotID, decimalLongitude,decimalLatitude, Accepted.Symbol,totalcover_sum,) %>%
+  mutate(CoverType = "PercentCover") %>% 
+  rename(Dataset=dataset, Long = decimalLongitude, Lat = decimalLatitude, SpCode=Accepted.Symbol, Cover=totalcover_sum, UniqueID = plotID)
+glimpse(neon_sub)
+range(neon_sub$Cover)
 
-# merge
-all = rbind(edd_class_sub, edd_cov_sub, edd_qual_sub, fl_sub, il_sub, imap_sub, nwca_sub, tx_sub, vahnp_sub, veg_sub)
+## merge
+all = rbind(edd_class_sub, edd_cov_sub, edd_qual_sub, fl_sub, il_sub, imap_sub, nwca_sub, tx_sub, vahnp_sub, veg_sub,
+            nps_sub, fia_sub, blm_sub, neon_sub)
 unique(all$Dataset)
 
 # make 0% cover measures and 'none' qualitative measures absences
@@ -87,3 +121,8 @@ table(all$CoverType) # cool!
 # add a column distinguishing presence and absence
 all = all %>% mutate(PA = ifelse(Cover=="Absent", "Absence", "Presence"))
 table(all$PA)
+
+# export an nceas merged datasets
+nceas = rbind(nps_sub, blm_sub, fia_sub, neon_sub)
+head(nceas)
+write.csv(nceas, "Latest_BLM_NPS_NEON_FIA_introduced.csv", row.names=F)
